9a660b758fe78dc8e3ba0e75a0bd678e40e3fb80
Odyhibit <josh.2990@gmail.com>
2022-08-13 16:18:52 -0500

    Saving package [date, name, version] to database. Script will try to pull 9 pages of 100. It will stop if a duplicate is found.

---
+++ webscraper/scrape_libraries_io.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+apikey = open("apikey", 'r').read().strip()
+most_recent = "2022-08-13T15:51:12+00:00"
+done = False
+con = sqlite3.connect('npm_packages.db')  # TABLE packages  ->  date text, name text,  version real
+
+
+def retrieve_page(page):
+    request_parameters = {
+        'page': str(page),
+        'per_page': "100",
+        'order': 'desc',
+        'platforms': 'NPM',
+        'sort': 'created_at',
+        'api_key': apikey}
+    resp = requests.get("https://libraries.io/search", params=request_parameters)
+    if not done:
+        print("URL is ", resp.url)
+        write_to_file(resp.text)
+
+
+def in_db(package: [], cursor: con.cursor()) -> bool:
+    global done
+    cursor.execute('SELECT count(date) FROM packages WHERE date = ? AND name = ? AND version = ?', package)
+    found = cursor.fetchone()
+    if found[0] > 0:
+        done = True
+        # print("found ", package, "so we are done.")
+        return True
+    return False
+
+
+def write_to_file(response):
+    soup = BeautifulSoup(response, 'html.parser')
+    cur = con.cursor()
+    for package in soup.find_all('div', 'project'):
+        name = package.find('a').get('href')
+        date = package.find('time').get('datetime')
+        version_text = package.find('small').getText()
+        version_start = 21  # manually counted
+        version_end = version_text.find('-')
+        version = version_text[version_start:version_end]
+        if not in_db([date, name, version], cur):
+            #  print("Inserting ", name)
+            cur.execute('INSERT INTO packages VALUES(?,?,?)', (date, name, version))
+
+    con.commit()
+
+
+for i in range(1, 10):
+    if not done:
+        retrieve_page(i)
+con.close()
