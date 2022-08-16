cddc6d8341bb06dc4b37b1ff3b35339df2821613
Odyhibit <josh.2990@gmail.com>
2022-08-15 22:57:32 -0500

    update existing packages with new date, and version instead of creating a new package entry for every version. Removed the '/NPM/' from the front of package names.

--- webscraper/scrape_libraries_io.py
+++ webscraper/scrape_libraries_io.py
@@ -8,23 +8,24 @@ import sqlite3
 
 
 def retrieve_page(page, connection):
+    global apikey
     request_parameters = {
         'api_key': apikey,
         'page': page,
         'per_page': "100",
         'order': 'desc',
         'platforms': 'NPM',
-        'sort': 'created_at',
-        }
+        'sort': 'created_at'}
     resp = requests.get("https://libraries.io/search", params=request_parameters)
-    if not done:
-        if resp.status_code != 200:
-            print(f"Status code is {resp.status_code}")
-            time.sleep(10)
-            retrieve_page(page, connection)
-        else:
-            print("Page", page, "URL is", resp.url, end="")
-            insert_new(resp.text, connection)
+
+    if resp.status_code == 200:
+        print("Page", page, "URL is", resp.url, end="")
+        insert_new(resp.text, connection)
+
+    if resp.status_code != 200:
+        print(f"Error retrieving page - status code is {resp.status_code}")
+        time.sleep(10)
+        retrieve_page(page, connection)
 
 
 def in_db(package: [], cursor: sqlite3.connect) -> bool:
@@ -39,11 +40,10 @@ def in_db(package: [], cursor: sqlite3.connect) -> bool:
 def insert_new(response, connection):
     global done
     soup = BeautifulSoup(response, 'html.parser')
-    con = connection
-    cur = con.cursor()
+    cur = connection.cursor()
     new, updated = 0, 0
     for package in soup.find_all('div', 'project'):
-        name = package.find('a').get('href')
+        name = package.find('a').get('href')[5:]
         date = package.find('time').get('datetime')
         version_text = package.find('small').getText()
         version_start = 21  # manually counted
@@ -59,17 +59,16 @@ def insert_new(response, connection):
     print("  * ", new, "new, ", updated, "updated packages")
     if new == 0:
         done = True
-    con.commit()
+    connection.commit()
 
 
 apikey = open("apikey", 'r').read().strip()
-most_recent = "2022-08-13T15:51:12+00:00"
 done = False
 con_db = sqlite3.connect('npm_packages.db')  # TABLE packages  ->  date text, name text,  version real
 
 for i in range(1, 101):  # any page past 100 gives a 404
     if not done:
-        if i % 9 == 0:   # rate limit seems to kick in after 10 pages
+        if i % 9 == 0:  # rate limit seems to kick in after 10 pages
             time.sleep(20)
         retrieve_page(i, con_db)
 
