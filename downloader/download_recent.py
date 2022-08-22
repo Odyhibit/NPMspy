18c10cf3efff76f4723c21859a1a7e28dcdbcf13
Odyhibit <josh.2990@gmail.com>
2022-08-21 20:07:38 -0500

    added first yara rule. need to implement gunzip, and scanning files. need more rules too

--- downloader/download_recent.py
+++ downloader/download_recent.py
@@ -1,8 +1,13 @@
+#  Josh Bloom
+# using Command line version of npm and yara.
+
+
 import sqlite3
 import subprocess
 
 import requests
 import yara
+import gzip
 
 
 def get_filename(name: str, version: str) -> str:
@@ -14,6 +19,10 @@ def get_filename(name: str, version: str) -> str:
 def get_download_url(name: str, version: str) -> str:
     return "https://registry.npmjs.org/" + name + "/-/" + get_filename(name, version)
 
+
+def extract_gzip(zip_file):
+    zip_file = gzip.open(zip_file)
+    return {name: zip_file.read(name) for name in zip_file.namelist()}
 
 conn_npm = sqlite3.connect("../webscraper/npm_packages.db")  # TABLE packages  ->  date text, name text,  version real
 cur_npm = conn_npm.cursor()
