e6dae52e8bb2db79844c82bd6f956c5c8aa5c93b
Odyhibit <josh.2990@gmail.com>
2022-09-20 08:00:07 -0500

    Socket.dev downloader works as a command line tool.

--- downloader/socket_dev_downloader.py
+++ downloader/socket_dev_downloader.py
@@ -3,26 +3,12 @@ import os
 import sys
 
 import requests
-from bs4 import BeautifulSoup
 
 
-def get_previous_version(selections: BeautifulSoup.find_all) -> str:
+def get_previous_version(selections: str) -> str:
     start = str(selections).find('option value="') + 14
     end = str(selections).find('"', start)
     return str(selections)[start:end]
-
-
-def get_new_soup(url: str) -> BeautifulSoup:
-    r = requests.get(url)
-    #  print(r.url)
-    return BeautifulSoup(r.text, 'html.parser')
-
-
-def write_file(name: str, url: str):
-    os.makedirs(os.path.dirname(name), exist_ok=True)
-    with open(name, "w+") as file_out:
-        content = requests.get(url)
-        file_out.write(str(content.content))
 
 
 def get_file_list(overview: str) -> list:
@@ -43,27 +29,34 @@ def get_content_url(description: str) -> str:
     return "https://socketusercontent.com/blob/" + str(description[start:end])
 
 
-package_name = str(sys.argv[1])
-print()
-print(f"Looking up {package_name} on socket.dev this may take a minute . . .")
-print()
-package_url = "https://socket.dev/npm/package/" + package_name + "/files"
+def write_file(name: str, url: str):
+    os.makedirs(os.path.dirname(name), exist_ok=True)
+    with open(name, "w+") as file_out:
+        content = requests.get(url)
+        file_out.write(content.content.decode("utf-8"))
 
-soup = get_new_soup(package_url)
-list_of_versions = soup.find_all('select', "chakra-select")
-if len(list_of_versions):
-    if "0.0.1-security" in str(list_of_versions):
-        previous_version = get_previous_version(list_of_versions)
+
+if __name__ == "__main__":
+
+    package_name = str(sys.argv[1])
+    print()
+    print(f"Looking up {package_name} on socket.dev this may take a minute . . .")
+    print()
+    package_url = "https://socket.dev/npm/package/" + package_name + "/files"
+
+    page_source = requests.get(package_url).text
+
+    if "0.0.1-security" in page_source:
+        previous_version = get_previous_version(page_source)
         package_url += "/" + str(previous_version)
-        soup = get_new_soup(package_url)
+        page_source = requests.get(package_url).text
 
-packages_overview = soup.find_all("script")[-1].text
-# print(get_file_list(packages_overview))
-list_of_items = get_file_list(packages_overview)
-for item in list_of_items:
-    filename = get_filename(item)
-    file_path = "package_downloads/" + package_name + "/" + filename
-    if len(filename) > 0:
-        content_url = get_content_url(item)
-        print(f"writing {file_path} {content_url}")
-        write_file(file_path, content_url)
+    list_of_items = get_file_list(page_source)
+
+    for item in list_of_items:
+        filename = get_filename(item)
+        file_path = "package_downloads/" + package_name + "/" + filename
+        if len(filename) > 0:
+            content_url = get_content_url(item)
+            print(f"writing {file_path}")
+            write_file(file_path, content_url)
